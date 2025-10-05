527e6695bf282380987ad2d6db9b9621e7ef0289
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-10-05 19:48:45 +0200

    Added button

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,18 +1,15 @@
 import * as React from 'react';
-import { AppRegistry } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import { ScrollView } from 'react-native';
+import { AppRegistry, ScrollView } from 'react-native';
+import { PaperProvider, Button } from 'react-native-paper';
 
 export default function App() {
   return (
     <PaperProvider>
-    
-      <ScrollView style={{ flex: 1, backgroundColor: '#f2f2f2' }}>
-        ….
+      <ScrollView style={{ flex: 1, backgroundColor: '#f2f2f2', padding: 16 }}>
+        <Button mode="contained" onPress={() => {}}>
+          Add 1 to Count
+        </Button>
       </ScrollView>
-
     </PaperProvider>
-
   );
 }
-
